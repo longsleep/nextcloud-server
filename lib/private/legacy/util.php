86dc98d305d37494e416c093d9e73e7a29bb4e86
Roeland Jago Douma <roeland@famdouma.nl>
2016-09-12 08:57:40 +0200

    Kill users with the reserved name on login

--- lib/private/legacy/util.php
+++ lib/private/legacy/util.php
@@ -314,7 +314,16 @@ class OC_Util {
 	 */
 	public static function copySkeleton($userId, \OCP\Files\Folder $userDirectory) {
 
-		$skeletonDirectory = \OCP\Config::getSystemValue('skeletondirectory', \OC::$SERVERROOT . '/core/skeleton');
+		$skeletonDirectory = \OC::$server->getConfig()->getSystemValue('skeletondirectory', \OC::$SERVERROOT . '/core/skeleton');
+		$instanceId = \OC::$server->getConfig()->getSystemValue('instanceid', '');
+
+		if ($instanceId === null) {
+			throw new \RuntimeException('no instance id!');
+		}
+		$appdata = 'appdata_' . $instanceId;
+		if ($userId === $appdata) {
+			throw new \RuntimeException('username is reserved name: ' . $appdata);
+		}
 
 		if (!empty($skeletonDirectory)) {
 			\OCP\Util::writeLog(
